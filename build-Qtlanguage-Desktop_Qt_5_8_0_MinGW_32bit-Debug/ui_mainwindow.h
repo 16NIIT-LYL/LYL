aeb107a7899927b3c2229c6f2daf2e440755da81
YWT1996 <466192448@qq.com>
2018-10-30 11:17:07 +0800

    up

---
+++ build-Qtlanguage-Desktop_Qt_5_8_0_MinGW_32bit-Debug/ui_mainwindow.h
@@ -0,0 +1,145 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.8.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionAbout_us;
+    QAction *actionEnglish;
+    QAction *actionChinese_Simplify;
+    QAction *actionChinese_Traditional;
+    QAction *actionJapanese;
+    QAction *actionGerman;
+    QAction *actionSave;
+    QAction *actionOpen;
+    QAction *actionCopy;
+    QWidget *centralWidget;
+    QPushButton *pushButton;
+    QMenuBar *menuBar;
+    QMenu *menuFile;
+    QMenu *menuEdit;
+    QMenu *menuHelp;
+    QMenu *menuLanguage;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(400, 300);
+        actionAbout_us = new QAction(MainWindow);
+        actionAbout_us->setObjectName(QStringLiteral("actionAbout_us"));
+        actionEnglish = new QAction(MainWindow);
+        actionEnglish->setObjectName(QStringLiteral("actionEnglish"));
+        actionChinese_Simplify = new QAction(MainWindow);
+        actionChinese_Simplify->setObjectName(QStringLiteral("actionChinese_Simplify"));
+        actionChinese_Traditional = new QAction(MainWindow);
+        actionChinese_Traditional->setObjectName(QStringLiteral("actionChinese_Traditional"));
+        actionJapanese = new QAction(MainWindow);
+        actionJapanese->setObjectName(QStringLiteral("actionJapanese"));
+        actionGerman = new QAction(MainWindow);
+        actionGerman->setObjectName(QStringLiteral("actionGerman"));
+        actionSave = new QAction(MainWindow);
+        actionSave->setObjectName(QStringLiteral("actionSave"));
+        actionOpen = new QAction(MainWindow);
+        actionOpen->setObjectName(QStringLiteral("actionOpen"));
+        actionCopy = new QAction(MainWindow);
+        actionCopy->setObjectName(QStringLiteral("actionCopy"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(140, 90, 101, 51));
+        pushButton->setStyleSheet(QStringLiteral("background-color: rgb(114, 159, 207);"));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 400, 20));
+        menuFile = new QMenu(menuBar);
+        menuFile->setObjectName(QStringLiteral("menuFile"));
+        menuEdit = new QMenu(menuBar);
+        menuEdit->setObjectName(QStringLiteral("menuEdit"));
+        menuHelp = new QMenu(menuBar);
+        menuHelp->setObjectName(QStringLiteral("menuHelp"));
+        menuLanguage = new QMenu(menuHelp);
+        menuLanguage->setObjectName(QStringLiteral("menuLanguage"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuFile->menuAction());
+        menuBar->addAction(menuEdit->menuAction());
+        menuBar->addAction(menuHelp->menuAction());
+        menuFile->addAction(actionOpen);
+        menuEdit->addAction(actionSave);
+        menuEdit->addAction(actionCopy);
+        menuHelp->addAction(actionAbout_us);
+        menuHelp->addSeparator();
+        menuHelp->addAction(menuLanguage->menuAction());
+        menuLanguage->addAction(actionEnglish);
+        menuLanguage->addAction(actionChinese_Simplify);
+        menuLanguage->addAction(actionChinese_Traditional);
+        menuLanguage->addAction(actionJapanese);
+        menuLanguage->addAction(actionGerman);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        actionAbout_us->setText(QApplication::translate("MainWindow", "About us", Q_NULLPTR));
+        actionEnglish->setText(QApplication::translate("MainWindow", "English", Q_NULLPTR));
+        actionChinese_Simplify->setText(QApplication::translate("MainWindow", "Chinese(Simplify)", Q_NULLPTR));
+        actionChinese_Traditional->setText(QApplication::translate("MainWindow", "Chinese(Traditional)", Q_NULLPTR));
+        actionJapanese->setText(QApplication::translate("MainWindow", "Japanese", Q_NULLPTR));
+        actionGerman->setText(QApplication::translate("MainWindow", "German", Q_NULLPTR));
+        actionSave->setText(QApplication::translate("MainWindow", "Save", Q_NULLPTR));
+        actionOpen->setText(QApplication::translate("MainWindow", "Open", Q_NULLPTR));
+        actionCopy->setText(QApplication::translate("MainWindow", "Copy", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("MainWindow", "Click", Q_NULLPTR));
+        menuFile->setTitle(QApplication::translate("MainWindow", "File", Q_NULLPTR));
+        menuEdit->setTitle(QApplication::translate("MainWindow", "Edit", Q_NULLPTR));
+        menuHelp->setTitle(QApplication::translate("MainWindow", "Help", Q_NULLPTR));
+        menuLanguage->setTitle(QApplication::translate("MainWindow", "Language", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
